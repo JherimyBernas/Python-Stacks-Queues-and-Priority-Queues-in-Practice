dadb1b1717c41ca588a9e7cec078d1cf7d543aae
JherimyBernas <jherimys.bernas@gmail.com>
2022-12-16 03:05:42 +0800

    Breadth-First Search Using a FIFO Queue

--- GRAPH.py
+++ GRAPH.py
@@ -32,7 +32,7 @@ class City(NamedTuple):
 
 
 
-def breadth_first_traverse(graph, source):
+def breadth_first_traverse1(graph, source):
     queue = Queue2(source)
     visited = {source}
     while queue:
@@ -42,7 +42,27 @@ def breadth_first_traverse(graph, source):
                 visited.add(neighbor)
                 queue.enqueue(neighbor)
 
-def breadth_first_search(graph, source, predicate):
-    for node in breadth_first_traverse(graph, source):
+def breadth_first_search1(graph, source, predicate):
+    for node in breadth_first_traverse1(graph, source):
+        if predicate(node):
+            return node
+
+
+
+def breadth_first_traverse2(graph, source, order_by=None):
+    queue = Queue2(source)
+    visited = {source}
+    while queue:
+        yield (node := queue.dequeue())
+        neighbors = list(graph.neighbors(node))
+        if order_by:
+            neighbors.sort(key=order_by)
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+
+def breadth_first_search2(graph, source, predicate, order_by=None):
+    for node in breadth_first_traverse2(graph, source, order_by):
         if predicate(node):
             return node
--- H_BreadthFirstSearchUsingFIFOQueue.py
+++ H_BreadthFirstSearchUsingFIFOQueue.py
@@ -15,6 +15,7 @@ for node in nx.bfs_tree(graph, nodes["edinburgh"]):
 print()
 
 
+
 def order(neighbors):
     def by_latitude(city):
         return city.latitude
@@ -27,5 +28,43 @@ for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
         break
     else:
         print("Not found")
+print()
+
+
+
+from GRAPH import (
+    City,
+    load_graph,
+    breadth_first_traverse1,
+    breadth_first_search1 as bfs,
+)
+def is_twentieth_century(city):
+    return city.year and 1901 <= city.year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+city = bfs(graph, nodes["edinburgh"], is_twentieth_century)
+city.name
+print()
+for city in breadth_first_traverse1(graph, nodes["edinburgh"]):
+    print(city.name)
+print()
+
+
+
+from GRAPH import (
+    City,
+    load_graph,
+    breadth_first_traverse2,
+    breadth_first_search2 as bfs,
+)
+def is_twentieth_century(city):
+    return city.year and 1901 <= city.year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+city = bfs(graph, nodes["edinburgh"], is_twentieth_century)
+city.name
+print()
+for city in breadth_first_traverse2(graph, nodes["edinburgh"]):
+    print(city.name)
 print()
 
