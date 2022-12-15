90155c3285816eab1225b6e98402b35df6d0a54b
JherimyBernas <jherimys.bernas@gmail.com>
2022-12-16 00:52:15 +0800

    Building a Queue Data Type

---
+++ BuildingQueueDataType.py
@@ -0,0 +1,20 @@
+print("\nPROGRAMMED BY: JHERIMY S. BERNAS")
+print("COURSE, YR. & SECTION: BSCOE 2-2\n")
+
+from BuildingQueueDataTypeClass import Queue1
+fifo = Queue1()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
+print(fifo.dequeue())
+print(fifo.dequeue())
+print(fifo.dequeue())
+
+print()
+from BuildingQueueDataTypeClass import Queue2
+fifo = Queue2("1st", "2nd", "3rd")
+len(fifo)
+for element in fifo:
+    print(element)
+len(fifo)
+print()
