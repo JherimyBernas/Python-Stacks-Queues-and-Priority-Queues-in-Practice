1d1373fac1e04982f3f65381cb20aedc6ec1f3fc
JherimyBernas <jherimys.bernas@gmail.com>
2022-12-16 23:02:10 +0800

    ROADMAP

--- QUEUES.py
+++ QUEUES.py
@@ -2,6 +2,7 @@ from collections import deque
 from heapq import heappop, heappush
 from itertools import count
 
+
 class Queue1:
     def __init__(self):
         self._elements = deque()
@@ -11,6 +12,7 @@ class Queue1:
 
     def dequeue(self):
         return self._elements.popleft()
+
 
 class Queue2:
     def __init__(self, *elements):
@@ -29,6 +31,7 @@ class Queue2:
     def dequeue(self):
         return self._elements.popleft()
 
+
 class Stack(Queue2):
     def dequeue(self):
         return self._elements.pop()
@@ -44,6 +47,7 @@ class PriorityQueue1:
     def dequeue(self):
         return heappop(self._elements)
 
+
 class PriorityQueue2:
     def __init__(self):
         self._elements = []
@@ -53,6 +57,7 @@ class PriorityQueue2:
 
     def dequeue(self):
         return heappop(self._elements)[1]
+
 
 class PriorityQueue3:
     def __init__(self):
