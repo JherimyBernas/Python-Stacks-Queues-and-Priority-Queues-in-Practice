880687d3ecb13f5cbf646aa58960b3937064de39
JherimyBernas <jherimys.bernas@gmail.com>
2022-12-16 23:55:25 +0800

    Thread Safe Queues

--- K_Dijkstra’sAlgorithmUsingPriorityQueue.py
+++ K_Dijkstra’sAlgorithmUsingPriorityQueue.py
@@ -17,7 +17,7 @@ def distance(weights):
 
 for city in dijkstra_shortest_path(graph, city1, city2, distance):
     print(city.name)
-
+print()
 
 def weight(node1, node2, weights):
     return distance(weights)
