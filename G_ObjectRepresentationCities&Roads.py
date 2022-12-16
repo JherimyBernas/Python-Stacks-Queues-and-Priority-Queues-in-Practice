1453ab0540c213c5c68db27a52c24b88d3c79150
JherimyBernas <jherimys.bernas@gmail.com>
2022-12-16 21:28:27 +0800

    Edit

--- G_ObjectRepresentationCities&Roads.py
+++ G_ObjectRepresentationCities&Roads.py
@@ -1,29 +1,29 @@
 import networkx as nx
 
+print("\n-----------------------------------------------------------------------------------------------------------\n")
 print(nx.nx_agraph.read_dot("roadmap.dot"))
-print()
 
+print("\n-----------------------------------------------------------------------------------------------------------\n")
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 print(graph.nodes["london"])
-print()
 
+print("\n-----------------------------------------------------------------------------------------------------------\n")
 from GRAPH import City, load_graph
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 print(nodes["london"])
 print()
-
 print(graph)
-print()
 
+print("\n-----------------------------------------------------------------------------------------------------------\n")
 for neighbor in graph.neighbors(nodes["london"]):
     print(neighbor.name)
-print()
 
+print("\n-----------------------------------------------------------------------------------------------------------\n")
 for neighbor, weights in graph[nodes["london"]].items():
     print(weights["distance"], neighbor.name)
 print()
 
-
+print("\n-----------------------------------------------------------------------------------------------------------\n")
 def sort_by(neighbors, strategy):
     return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
 
